35e94f5de13c53a79c879054a0242051009dafb9
m_emin <mehmeteminbul@gmail.com>
2022-07-20 21:22:14 +0300

    first

---
+++ elta_task/src/components/wrapperContainer.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export default styled.div`
+    display: grid;
+    grid-template-areas: "nav content content content"
+                         "nav content content content"                        
+                         "nav content content content";
+    width: 50%;
+    height: 30rem;
+    background-color: whitesmoke;
+`
---
+++ elta_task/src/components/wrapperContent.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default styled.div`
+    width: 100%;
+    grid-area: content;
+    justify-content: center;
+    background-color: whitesmoke;
+    margin-left: 1rem;
+    `
